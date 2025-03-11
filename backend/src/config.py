342ef54c16ee4728565730ee7e8746328c36bf2c
e <sleithe@cardiff.ac.uk>
2025-03-11 08:33:27 +0000

    Added Requirements.txt and example .env file

--- backend/src/config.py
+++ backend/src/config.py
@@ -7,7 +7,7 @@ class Config(BaseSettings):
     database_url: str
     database_echo_all: bool = False
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env.dev",env_file_encoding="utf-8")
 
 config = Config()   # TODO: Error Handling For Missing Configuration
 
