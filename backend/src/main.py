8526cfd94a3b5538b5224e4494c7119295ee8de9
E <sleithe@cardiff.ac.uk>
2025-03-12 17:34:13 +0000

    Refactoring, added example protected route.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from database import init_db
-from routes.auth import auth_router
+from api.routes.user import user_router
+from api.routes.auth import auth_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")
+app.include_router(user_router, prefix="/user")
 
 @app.on_event("startup")
 def initalize_db():
