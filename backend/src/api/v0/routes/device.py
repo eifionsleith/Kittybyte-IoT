e7df1c33f0b28899d0a51a6fcfd3e2607a675982
E <sleithe@cardiff.ac.uk>
2025-03-24 20:17:25 +0000

    (!) Insecure MQTT Dispense Topic

--- backend/src/api/v0/routes/device.py
+++ backend/src/api/v0/routes/device.py
@@ -7,6 +7,7 @@ Devices:
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+import paho.mqtt.publish as publish
 
 from api.v0.dependencies.auth import get_user_from_jwt
 from api.v0.dependencies.database import get_db
@@ -14,6 +15,7 @@ from models.device import Device
 from models.user import User
 from crud.device import device_crud_interface
 from schema.device import DeviceUpdate, DeviceCreate
+from utils.mqtt import publish_single
 
 router = APIRouter()
 
@@ -64,3 +66,31 @@ def unregister_device(
     device_update = DeviceUpdate(name=None, owner_id=None)
     device_crud_interface.update(db, device, device_update)
     return {"message": "Device unregistered successfully."}
+
+@router.post("/{device_id}/dispense")
+def dispense(
+        db: Annotated[Session, Depends(get_db)],
+        current_user: Annotated[User, Depends(get_user_from_jwt)],
+        device_id: int):
+    device = device_crud_interface.get_one(db, Device.id == device_id)
+
+    if not device:
+        raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No such device.")
+
+    if device.owner_id != current_user.id:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User lacks permissions to acces this device.")
+
+    topic = f"device/{device.id}/dispense"
+    payload = "trigger"
+    try:
+        publish_single(topic, payload)
+        return {"message": "Dispense command sent!"}
+    except Exception:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Internal Server Error")
+
