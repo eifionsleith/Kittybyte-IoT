2527758f1ef17fd864aac2598ac5c683b35bec33
Justin Woodham <woodhamj@cardiff.ac.uk>
2025-04-07 13:22:48 +0000

    Update object-ident-2.py to include 2fps and now print true false

--- pi/AI Detection (using COCO library)/object-ident-2.py
+++ pi/AI Detection (using COCO library)/object-ident-2.py
@@ -40,6 +40,7 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
 if __name__ == "__main__":
 
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 2)
     cap.set(3,640)
     cap.set(4,480)
     #cap.set(10,70)
@@ -48,6 +49,7 @@ if __name__ == "__main__":
     while True:
         success, img = cap.read()
         result, objectInfo = getObjects(img,0.45,0.2, objects=['cat'])
-        #print(objectInfo)
+        print(True if objectInfo else False)
+
         cv2.imshow("Output",img)
         cv2.waitKey(1)
