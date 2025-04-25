e4c6e38c11721e3cbe25ef32b5a8b5752947bc50
E <sleithe@cardiff.ac.uk>
2025-04-25 10:41:37 +0100

    Before update melody loop

--- device/arduino/src/actuators/BuzzerController.cpp
+++ device/arduino/src/actuators/BuzzerController.cpp
@@ -4,11 +4,42 @@
 
 namespace BuzzerController {
   namespace {
+    // True max size within the packet would be 29.
+    // If packet size or structure changes this would need updating.
+    const byte MAX_MELODY_NOTES = 30;
+
+    struct CurrentMelody {
+      uint16_t tempo = 0;
+      uint16_t frequencies[MAX_MELODY_NOTES];
+      byte length = 0;
+      uint16_t base_not_duration = 0;
+      byte current_frequency_index = 0;
+    };
+
+    byte buzzer_pin = -1;
     BuzzerState current_state = BuzzerState::IDLE;
     unsigned long task_end_time = 0;
-    const uint16_t* current_melody_notes = nullptr;
-    const uint16_t current_melody_index = 0;
-    byte buzzer_pin = -1;
+
+    CurrentMelody active_melody;
+
+    // Functions to handle each state, called each cycle by update(), based on current state.
+    void handle_simple_buzz_state() {
+      unsigned long now = millis();
+      if (now > task_end_time) {
+        noTone(buzzer_pin);
+        current_state = BuzzerState::IDLE;
+        Protocol::send_response(Commands::NOTIFY_TASK_COMPLETE, nullptr, 0);
+      }
+    }
+
+    void handle_playing_melody_state() { // TODO!!!!
+      unsigned long now = millis();
+      if (now > task_end_time) {
+        // Move to the next note... if there is one...
+        noTone(buzzer_pin);
+
+      }
+    }
   }
 
   void init(byte buzzer_pin_to_set) {
@@ -47,6 +78,24 @@ namespace BuzzerController {
     current_state = BuzzerState::SIMPLE_DURATION;
     task_end_time = millis() + duration_ms;
     tone(buzzer_pin, frequency);
+    return true;
+  }
+
+  bool start_melody(uint16_t tempo, const uint16_t* frequencies, byte frequencies_l) {
+    if (current_state != BuzzerState::IDLE || buzzer_pin == (byte) -1) {
+      return false;
+    }
+
+    active_melody.tempo = tempo;
+    memcpy(active_melody.frequencies, frequencies, frequencies_l * sizeof(uint16_t));
+    active_melody.length = frequencies_l;
+    active_melody.current_frequency_index = 0;
+    active_melody.base_not_duration = 60000 / active_melody.tempo;
+    
+    current_state = BuzzerState::PLAYING_MELODY;
+    tone(buzzer_pin, active_melody.frequencies[0]);
+    task_end_time = millis() + active_melody.base_not_duration;
+
     return true;
   }
 }
--- device/arduino/src/actuators/BuzzerController.h
+++ device/arduino/src/actuators/BuzzerController.h
@@ -18,7 +18,7 @@ namespace BuzzerController {
   bool start_simple_buzz(uint16_t frequency, uint16_t duration_ms);
 
   // Sets the state to indicate the start of a melody
-  bool start_melody(const uint16_t* frequencies, const uint16_t frequencies_length);
+  bool start_melody(const uint16_t tempo, const uint16_t* frequencies, const byte frequencies_l);
 
   // Returns the state - good for checking if IDLE, and therefore ready to recieve commands
   BuzzerState get_current_state();
